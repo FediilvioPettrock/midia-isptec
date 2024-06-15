d72f7b534775eaa3edef0d4398d3f8819b588a29
kudiezo <kudiezo527@gmail.com>
2024-06-15 23:56:41 +0100

    end of the day

--- src/pages/Cadastro.js
+++ src/pages/Cadastro.js
@@ -1,67 +1,68 @@
 import styles from './Cadastro.module.css';
 import Linker from '../components/Linker'
 import Navbar from '../components/Navbar';
-function Cadastro(){
-    return(
+import { useNavigate } from "react-router-dom";
+import { register } from '../server/authentication';
+import { useState } from 'react';
+
+function Cadastro() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [username, setUsername] = useState(''); // Adicionando categoria ao estado
+    const [role, seRole] = useState(''); // Adicionando categoria ao estado
+    const navigate = useNavigate();
+
+    const handleUserRegister = async (event) => {
+        event.preventDefault();
+        try { 
+            const data = await register (email, password, username, null);
+            console.log(data);
+            navigate("/userpage", { replace: true });
+        } catch (error) {
+            console.error(error);
+        }
+        // if (categoria === "Cliente") {
+        //     localStorage.setItem("authenticated", true);
+        //     navigate("/userpage", { replace: true });
+        // }
+    };
+
+    return (
         <div className={styles.Cadastro_container}>
-        <Navbar/>
+            <div className={styles.Rigth_div}>
+                <h2>Registo</h2>
+                <form onSubmit={handleUserRegister}>
+                    <div className={styles.Container1}>
+                        <div className={styles.Separador}>
+                            <label htmlFor="username">Username</label>
+                            <input 
+                                type='text' name='username' value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                />
+                        </div>
+                        <div className={styles.Separador}>
+                            <label htmlFor="email">Email</label>
+                            <input type='email' name='email' value={email}
+                                onChange={(e) => setEmail(e.target.value)}/>
+                        </div>
+                    </div>
 
-        <div className={styles.Rigth_div}>
-            <h2>Registo</h2>
-            <form>
-            <div className={styles.Container1}>
-                <div className={styles.Separador}>
-                    <label for="nome">Nome Completo</label>
-                    <input type='text'/>
-                </div>
-               <div className={styles.Separador}>
-               <label for="username">Username</label>
-                <input type='text'/>
-               </div>
-            </div>
-            
-            <div className={styles.Container1}>
-                <div className={styles.Separador}>
-                <label for="email">Email</label>
-                <input type='email'/>
-                </div>
-               <div className={styles.Separador}>
-               <label for="Telefone">Contacto</label>
-                <input type='text'/>                
-               </div>
-            </div>
-   
-            <div className={styles.Container1}>
-                <div className={styles.Separador}>
-                    <label for="bi">Numero do BI</label>
-                    <input type='text'/>
-                </div>
-               <div className={styles.Separador}>
-                    <label for="Foto">Foto</label>
-                    <input type='file'/>               
-               </div>
-            </div>
+                    <div className={styles.Container1}>
+                        <div className={styles.Separador}>
+                            <label htmlFor="password">Password</label>
+                            <input type='password' name='password' value={password}
+                        onChange={(e) => setPassword(e.target.value)}/>
+                        </div>
+                    </div>
 
-            <div className={styles.Container1}>
-                <div className={styles.Separador}>
-                    <label for="password">Password</label>
-                    <input type='password'/>
-                </div>
-
-               <div className={styles.Separador}>
-                    <label for="confirmacao">Confirme a Password</label>
-                    <input type='password'/>             
-               </div>
+                    <div className={styles.SendButton}>
+                        <button type="submit">Register</button>
+                    </div>
+                </form>
+                <hr />
+                <Linker rota="/Login" message="Já possui uma conta?" />
             </div>
-
-                <div className={styles.SendButton}>
-                  <button type="submit">Registar</button>    
-                </div>
-            </form>
-            <hr/>
-            <Linker rota="/Login" message="Já possui uma conta?"/>
         </div>
-    </div> 
     )
 }
 
